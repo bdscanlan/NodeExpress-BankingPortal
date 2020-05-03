06aa86ea1d3591adbc68c83d0b0e109bca854dc4
Bruce <bruce_scanlan@manulife.ca>
2020-05-03 10:24:22 -0400

    aasda

--- src/app.js
+++ src/app.js
@@ -1,12 +1,14 @@
 const fs = require('fs');
 const path = require('path');
+
 const express = require('express');
 
 const app = express();
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-app.set(express.static(path.join(__dirname, 'public')));
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
   res.render('index', { title: 'Index' });
