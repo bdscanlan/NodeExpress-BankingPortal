3fc2f36141de104399498e7d3177c00c209c82dc
Bruce <bruce_scanlan@manulife.ca>
2020-05-03 12:00:30 -0400

    444d

--- src/app.js
+++ src/app.js
@@ -10,16 +10,28 @@ app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-const accountData = fs.readFileSync(path.join(__dirname, 'src/json/accounts.json'), 'UTF8');
+const accountData = fs.readFileSync(path.join(__dirname, 'json/accounts.json'), 'UTF8');
 const accounts = JSON.parse(accountData);
 
-const userData = fs.readFileSync(path.join(__dirname, 'src/json/users.json'), 'UTF8');
+const userData = fs.readFileSync(path.join(__dirname, 'json/users.json'), 'UTF8');
 const users = JSON.parse(userData);
 
-
 app.get('/', (req, res) => {
-  res.render('index', { title: 'Index' });
+  res.render('index', { title: 'Account Summary', accounts: accounts });
 });
+
+app.get('/savings', (req, res) => {
+  res.render('account', { account: accounts.savings });
+});
+
+app.get('/checking', (req, res) => {
+  res.render('account', { account: accounts.checking });
+});
+
+app.get('/credit', (req, res) => {
+  res.render('account', { account: accounts.credit });
+});
+
 
 app.listen(3000, () => {
   console.log(`Listening to requests on http://localhost:3000`);
