a8f5466d0ce5fee3c9a51b89a378b8c578844cea
Bruce <bruce_scanlan@manulife.ca>
2020-05-03 09:59:08 -0400

    aasda

---
+++ src/app.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+const path = require('path');
+const express = require('express');
+
+const app = express();
+
+app.set('views', path.join(__dirname, '../views'));
+app.set('view engine', 'ejs');
+app.set(express.static(path.join(__dirname, '../public')));
+
+app.get('/', (req, res) => {
+  res.render('index', { title: 'Index' });
+});
+
+app.listen(port, () => {
+  console.log(`Listening to requests on http://localhost:3000`);
+});
