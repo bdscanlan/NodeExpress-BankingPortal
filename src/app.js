93319b2a54e2549be0770341e1dcff57318c160d
Bruce <bruce_scanlan@manulife.ca>
2020-05-03 12:10:21 -0400

    aasda

--- src/app.js
+++ src/app.js
@@ -32,6 +32,10 @@ app.get('/credit', (req, res) => {
   res.render('account', { account: accounts.credit });
 });
 
+app.get('/profile', (req, res) => {
+  res.render('profile', { user: users[0] });
+});
+
 
 app.listen(3000, () => {
   console.log(`Listening to requests on http://localhost:3000`);
