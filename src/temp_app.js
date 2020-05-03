9852313cdf55434b277a5fe4fd0df55f5358367d
Bruce <bruce_scanlan@manulife.ca>
2020-05-03 10:33:17 -0400

    aasda

--- src/temp_app.js
+++ src/temp_app.js
@@ -13,14 +13,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 const accountData = fs.readFileSync(path.join(__dirname, 'src/json/accounts.json'), 'UTF8');
 const accounts = JSON.parse(accountData);
 
-const userData = fs.readFileSync(path.join(__dirname, 'src/json/users.json'), 'UTF8');
-const users = JSON.parse(userData);
-
-
 app.get('/', (req, res) => {
   res.render('index', { title: 'Index' });
 });
 
-app.listen(3000, () => {
-  console.log(`Listening to requests on http://localhost:3000`);
-});
+app.listen(3000);
